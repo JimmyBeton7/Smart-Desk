c2c1160433b7bb32b23e74849d3af2d020fe6c7b
JimmyBeton7 <mateusz.durka77@gmail.com>
2025-07-13 16:59:20 +0200

    v4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
     <div>
       <h1>Welcome to Smart Desk Companion</h1>
       <p>Select a tool from the sidebar.</p>
+      <p>Version: 1.0.2.</p>
 
       <div className="tiles-container">
         <WeatherTile />
